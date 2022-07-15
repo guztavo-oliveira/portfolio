1479fd7a05eaeab7cc639c3c5dc52107194fd94e
Gustavo <gu.machado.oliveira@gmail>
2022-07-15 17:49:04 -0300

    feat: create component PortfolioCard

---
+++ src/Components/PortfolioCard/index.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from "react";
+import { Container } from "./style";
+import { Tecnologies } from "../../Utils/Tecnologies";
+import { Button } from "../Button";
+
+interface IProjects {
+  name: string;
+  description: string;
+  techs: string[];
+  github: string;
+  image: string;
+  demo: string;
+}
+interface IPortfolioCardProps {
+  children?: ReactNode;
+  project: IProjects;
+}
+
+const PortfolioCard = ({ project }: IPortfolioCardProps) => {
+  return (
+    <Container>
+      {/* <div className="card_logo_tag">Backend</div> */}
+      <h1>{project.name}</h1>
+
+      <div className="card_container">
+        <div className="card_logo">
+          <img
+            src={project.image}
+            className="project_logo"
+            alt="logo of the respective project"
+          />
+        </div>
+        <div className="card_content">
+          <p>{project.description}</p>
+          <div className="container__buttons">
+            <a href={project.github} target="_blank" rel="noreferrer">
+              <Button>{"GitHub"}</Button>
+            </a>
+            <a href={project.demo} target="_blank" rel="noreferrer">
+              <Button>{"Demo"}</Button>
+            </a>
+          </div>
+          <ul>
+            {project.techs.map((e) => {
+              return (
+                <li>
+                  <abbr title={e}>
+                    <img src={Tecnologies[e]} alt={e} />
+                  </abbr>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </Container>
+  );
+};
+export { PortfolioCard };
---
+++ src/Components/PortfolioCard/style.ts
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1000px;
+  /* height: 400px; */
+  /* border: 2px solid chartreuse; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(48, 48, 48, 0.3);
+  padding: 20px;
+  border-radius: 20px;
+  transition: ease 0.5s;
+  position: relative;
+
+  /* .card_logo_tag {
+    position: absolute;
+    color: red;
+    font-size: 15px;
+    left: 36%;
+    top: 9%;
+  } */
+
+  &:hover {
+    transform: translate(-2px, -2px);
+    transition: ease 0.5s;
+    box-shadow: 10px 10px 0.4rem rgba(0, 0, 0, 0.3);
+  }
+
+  h1 {
+    background-image: linear-gradient(
+      183.51deg,
+      rgb(96, 255, 217) 0%,
+      rgb(0, 144, 118) 100%
+    );
+    -webkit-text-fill-color: transparent;
+    font-size: 1.5rem;
+    background-clip: text;
+  }
+
+  p {
+    font-size: 0.8rem;
+    line-height: 1rem;
+    padding: 0px 10px;
+    text-align: justify;
+    color: #e6f1ff;
+  }
+
+  .card_container {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+    justify-content: center;
+
+    @media (min-width: 580px) {
+      display: flex;
+      flex-direction: row;
+      margin-top: 10px;
+      align-items: center;
+    }
+  }
+
+  .card_logo {
+    display: flex;
+    justify-content: center;
+
+    img {
+      margin: 10px 0;
+      width: 90%;
+      border-radius: 10px;
+    }
+  }
+
+  .card_content {
+    .container__buttons {
+      display: flex;
+      gap: 20px;
+      margin: 10px 0;
+      padding: 20px;
+      justify-content: center;
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      width: 90%;
+      gap: 10px;
+      justify-content: center;
+      width: 100%;
+
+      img {
+        height: 50px;
+      }
+    }
+
+    @media (min-width: 580px) {
+      max-width: 40%;
+      min-width: 40%;
+    }
+  }
+`;
