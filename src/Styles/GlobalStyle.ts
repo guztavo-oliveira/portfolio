a3812139686097848f6f8b0842ce5ed9002d0d9b
Gustavo <gu.machado.oliveira@gmail>
2022-07-15 17:43:37 -0300

    feat: create essencial folders

---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+@font-face {
+      font-family: "tweet";
+      src: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/277/tweet.woff")
+        format("woff");
+      font-weight: normal;
+      font-style: normal;
+    }
+`;
