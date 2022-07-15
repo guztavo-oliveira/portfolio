782120df40807381b647013709e810100095ed7b
Gustavo <gu.machado.oliveira@gmail>
2022-07-15 17:57:01 -0300

    feat: create Projects and Technologies files

---
+++ src/Utils/Projects.ts
@@ -0,0 +1,42 @@
+import findRecipes from "../assets/images/find-recipes.png";
+import ajudeMeuPet from "../assets/images/ajude-meu-pet.png";
+
+interface IProjects {
+  name: string;
+  description: string;
+  techs: string[];
+  github: string;
+  image: string;
+  demo: string;
+}
+
+export const Projects = [
+  {
+    name: "Find my recipe",
+    description:
+      "Desenvolver os endpoint da API REST, CRUD do usuário e cadastro de itens, para servir a aplicação" +
+      "que já possuía o frontend implementado.",
+    techs: ["python", "sqlAlchemy", "flask", "postgreSQL", "heroku"],
+    image: findRecipes,
+    github: "https://github.com/guztavo-oliveira/capstone-q3-find-my-recipes",
+    demo: "https://capstone-dusky.vercel.app/",
+  },
+  {
+    name: "Ajude meu pet",
+    description:
+      "Aplicação pensada para auxiliar os donos de animais a manter os cuidados de seu companheiro em" +
+      "dia. Através do cadastro no site é possível contratar os serviços de passeio, treinamentos e de táxi" +
+      "pet, caso seja necessário levá-lo ao veterinário ou a outro local",
+    techs: [
+      "react",
+      "materialUI",
+      "styledComponents",
+      "jest",
+      "cypress",
+      "vercel",
+    ],
+    image: ajudeMeuPet,
+    github: "https://github.com/guztavo-oliveira/capstone-q2-ajude-meu-pet",
+    demo: "https://capstone-q2-ajude-meu-pet-zeta.vercel.app/",
+  },
+] as IProjects[];
---
+++ src/Utils/Tecnologies.ts
@@ -0,0 +1,53 @@
+import cy from "../assets/icons/cypress.png";
+import html from "../assets/icons/html5.png";
+import css from "../assets/icons/css3.png";
+import python from "../assets/icons/python.png";
+import postgresql from "../assets/icons/postgresql.png";
+import flask from "../assets/icons/flask.png";
+import sqlalchemy from "../assets/icons/sqlalchemy.svg";
+import materialui from "../assets/icons/materialui.svg";
+import react from "../assets/icons/react.png";
+import jest from "../assets/icons/jest.png";
+import heroku from "../assets/icons/heroku.png";
+import sc from "../assets/icons/styledcomponents.png";
+import vercel from "../assets/icons/vercel.png";
+import js from "../assets/icons/javascript.png";
+import node from "../assets/icons/nodejs.png";
+import docker from "../assets/icons/docker.png";
+import express from "../assets/icons/express.png";
+import django from "../assets/icons/django.png";
+import mongo from "../assets/icons/mongo.png";
+import git from "../assets/icons/git.png";
+import trello from "../assets/icons/trello.png";
+import typescript from "../assets/icons/typescript.png";
+import notion from "../assets/icons/notion.png";
+
+interface ITecnologies {
+  [key: string]: any;
+}
+
+export const Tecnologies = {
+  cypress: cy,
+  html5: html,
+  css3: css,
+  javascript: js,
+  python: python,
+  nodejs: node,
+  postgreSQL: postgresql,
+  flask: flask,
+  sqlAlchemy: sqlalchemy,
+  heroku: heroku,
+  react: react,
+  materialUI: materialui,
+  jest: jest,
+  styledComponents: sc,
+  vercel: vercel,
+  docker: docker,
+  express: express,
+  django: django,
+  mongo: mongo,
+  git: git,
+  trello: trello,
+  typescript: typescript,
+  notion: notion,
+} as ITecnologies;
